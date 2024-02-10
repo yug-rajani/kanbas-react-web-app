7a5eedc57d9ab3c02950d39f8475223c73d5ffaa
Yug Rajani <yug.rajani99@gmail.com>
2024-02-10 11:39:25 -0500

    Complete lab 2.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Kanbas from './Kanbas';
+import Labs from './Labs';
+import HelloWorld from './Labs/a3/HelloWorld';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Labs />
+      <Kanbas />
+      <HelloWorld />
     </div>
   );
 }
