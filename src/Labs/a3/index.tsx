49428c310ca3927aa6db6b857640e877cf058e4d
Yug Rajani <yug.rajani99@gmail.com>
2024-02-17 18:43:12 -0500

    Complete till 2.6

--- src/Labs/a3/index.tsx
+++ src/Labs/a3/index.tsx
@@ -2,11 +2,13 @@ import Classes from "./Classes";
 import JavaScript from "./JavaScript";
 import PathParameters from "./routing/PathParameters";
 import Styles from "./Styles";
+import ConditionalOutput from "./ConditionalOutput";
 
 function Assignment3() {
     return (
         <div className="container">
             <h1>Assignment 3</h1>
+            <ConditionalOutput />
             <Styles />
             <Classes />
             <PathParameters />
