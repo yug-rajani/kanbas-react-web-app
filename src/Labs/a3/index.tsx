4ecdb3a9dea2646788e9548b713c241703a2b053
Yug Rajani <yug.rajani99@gmail.com>
2024-02-17 19:36:46 -0500

    Complete lab

--- src/Labs/a3/index.tsx
+++ src/Labs/a3/index.tsx
@@ -3,6 +3,9 @@ import JavaScript from "./JavaScript";
 import PathParameters from "./routing/PathParameters";
 import Styles from "./Styles";
 import ConditionalOutput from "./ConditionalOutput";
+import Highlight from "./Highlight";
+import Add from "./Add";
+import TodoList from "./todos/TodoList";
 
 function Assignment3() {
     return (
@@ -13,6 +16,12 @@ function Assignment3() {
             <Classes />
             <PathParameters />
             <JavaScript />
+            <Highlight>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipitratione eaque illo minus cum, saepe totam
+                vel nihil repellat nemo explicabo excepturi consectetur. Modi omnis minus sequi maiores, provident voluptates.
+            </Highlight>
+            <Add a={3} b={4} />
+            <TodoList />
         </div>
     );
 }
