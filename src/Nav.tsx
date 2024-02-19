4886b5122f8f69f5cd6219b4f321072ca061dfbb
Yug Rajani <yug.rajani99@gmail.com>
2024-02-19 11:25:52 -0500

    Update main nav label

--- src/Nav.tsx
+++ src/Nav.tsx
@@ -4,7 +4,7 @@ function Nav() {
     const { pathname } = useLocation();
     return (
         <nav className="nav nav-tabs mt-2">
-            <Link className={`nav-link ${pathname.includes("a3") ? "active" : ""}`} to="/Labs/a3">A3</Link>
+            <Link className={`nav-link ${pathname.includes("a3") ? "active" : ""}`} to="/Labs/a3">Labs</Link>
             <Link className={`nav-link ${pathname.includes("Kanbas") ? "active" : ""}`} to="/Kanbas">Kanbas</Link>
             <Link className={`nav-link ${pathname.includes("hello") ? "active" : ""}`} to="/hello">Hello</Link>
         </nav>
