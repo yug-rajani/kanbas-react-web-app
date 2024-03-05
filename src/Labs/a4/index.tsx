59818ab57be56448f5b56fb583a97f194b2b1082
Yug Rajani <yug.rajani99@gmail.com>
2024-03-05 09:43:37 -0500

    Complete 2.3

--- src/Labs/a4/index.tsx
+++ src/Labs/a4/index.tsx
@@ -1,14 +1,38 @@
+import ArrayStateVariable from "./ArrayStateVariable";
+import BooleanStateVariables from "./BooleanStateVariables";
 import ClickEvent from "./ClickEvent";
+import Counter from "./Counter";
+import DateStateVariable from "./DateStateVariable";
+import EventObject from "./EventObject";
+import ObjectStateVariable from "./ObjectStateVariable";
+import ParentStateComponent from "./ParentStateComponent";
 import PassingDataOnEvent from "./PassingDataOnEvent";
+import PassingFunctions from "./PassingFunctions";
+import ReduxExamples from "./ReduxExamples";
+import StringStateVariables from "./StringStateVariables";
 // import PassingFunctions from "./PassingFunctions";
 
 function Assignment4() {
+    function sayHello() {
+        alert("Hello");
+    }
+
     return (
         <div className="container">
             <h1>Assignment 4</h1>
+            <ReduxExamples />
+
             <ClickEvent />
             <PassingDataOnEvent />
-            {/* <PassingFunctions /> */}
+            <PassingFunctions theFunction={sayHello} />
+            <EventObject />
+            <Counter />
+            <BooleanStateVariables />
+            <StringStateVariables />
+            <DateStateVariable />
+            <ObjectStateVariable />
+            <ArrayStateVariable />
+            <ParentStateComponent />
         </div>
     );
 }
