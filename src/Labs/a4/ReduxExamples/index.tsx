069d1727d7ecd8ce9c825de214569eaf18f40f64
Yug Rajani <yug.rajani99@gmail.com>
2024-03-14 20:09:48 -0400

    Complete 2.4, start with 2.5

--- src/Labs/a4/ReduxExamples/index.tsx
+++ src/Labs/a4/ReduxExamples/index.tsx
@@ -1,9 +1,15 @@
 import React from "react";
+import HelloRedux from "./HelloRedux";
+import CounterRedux from "./CounterRedux";
+import AddRedux from "./AddRedux";
 
 const ReduxExamples = () => {
     return (
         <div>
             <h2>Redux Examples</h2>
+            <HelloRedux />
+            <CounterRedux />
+            <AddRedux />
         </div>
     );
 };
--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -2,16 +2,20 @@ import { Route, Routes } from "react-router-dom";
 import Nav from "../Nav";
 import Assignment3 from "./a3";
 import Assignment4 from "./a4";
+import store from "./store";
+import { Provider } from "react-redux";
 
 function Labs() {
     return (
-        <div className="container-fluid">
-            <Nav />
-            <Routes>
-                <Route path="/a3/*" element={<Assignment3 />} />
-                <Route path="/a4" element={<Assignment4 />} />
-            </Routes>
-        </div>
+        <Provider store={store}>
+            <div className="container-fluid">
+                <Nav />
+                <Routes>
+                    <Route path="/a3/*" element={<Assignment3 />} />
+                    <Route path="/a4" element={<Assignment4 />} />
+                </Routes>
+            </div>
+        </Provider>
     );
 }
 export default Labs;
