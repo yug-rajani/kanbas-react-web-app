1bc7e29beab61cac46798f91f71a43985d47b9a0
Yug Rajani <yug.rajani99@gmail.com>
2024-03-17 21:27:59 -0400

    Refactor as per 3.2 - move the state variable to parent

--- src/Kanbas/Dashboard/index.tsx
+++ src/Kanbas/Dashboard/index.tsx
@@ -1,44 +1,14 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
-import * as db from "../Database";
 import { FaComment, FaFolder, FaPenSquare } from "react-icons/fa";
-function Dashboard() {
-    const [courses, setCourses] = useState(db.courses);
-    const [course, setCourse] = useState({
-        _id: "0",
-        name: "New Course",
-        number: "New Number",
-        startDate: "2023-09-10",
-        endDate: "2023-12-15",
-        semester: "New Semester",
-        textbook: "New Course Textbook",
-        image: "reactjs.jpg"
-    });
-
-    const addNewCourse = () => {
-        const newCourse = {
-            ...course,
-            _id: new Date().getTime().toString()
-        };
-        setCourses([...courses, { ...course, ...newCourse }]);
-    };
-
-    const deleteCourse = (courseId: string) => {
-        setCourses(courses.filter((course) => course._id !== courseId));
-    };
-
-    const updateCourse = () => {
-        setCourses(
-            courses.map((c) => {
-                if (c._id === course._id) {
-                    return course;
-                } else {
-                    return c;
-                }
-            })
-        );
-    };
-
+function Dashboard(
+    { courses, course, setCourse, addNewCourse,
+        deleteCourse, updateCourse }: {
+            courses: any[]; course: any; setCourse: (course: any) => void;
+            addNewCourse: () => void; deleteCourse: (course: any) => void;
+            updateCourse: () => void;
+        }
+) {
     return (
         <div className="p-4">
             <h1>Dashboard</h1>
